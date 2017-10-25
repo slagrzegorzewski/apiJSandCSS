269e12ab82ab449f4e6f08dcad709f569fe85bf0
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-10-25 22:33:58 +0200

    Add image, add event lsiteners for inputs

--- src.js
+++ src.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   console.log("test");
+  const inputs = document.querySelectorAll(".options input");
+
+  function handleUpdate() {
+    console.log(this.value);
+  }
+
+  inputs.forEach(input => input.addEventListener("change", handleUpdate));
+  inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+
 });
